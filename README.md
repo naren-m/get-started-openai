c5b8ba6d2cd245025fe437614f544d753d98ff29
Naren Mudivarthy <nmudivar@cisco.com>
2017-06-04 19:29:38 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # get-started-openai
 Starting points for open ai gym and agents
+
+## Getting started
+
+### Creating conda environment for open universe
+
+``` sh
+# Environment
+conda create --name rl
+source activate rl
+
+# Installing pre-reqs
+## On Mac
+brew install tmux htop cmake golang libjpeg-turbo
+## On Linux
+use sudo apt-get install -y tmux htop cmake golang libjpeg-dev
+
+pip install "gym[atari]"
+pip install universe
+pip install six
+pip install tensorflow
+conda install -y -c https://conda.binstar.org/menpo opencv3
+conda install -y numpy
+conda install -y scipy
+```
+
+### Testing environment
+
+```sh
+git clone https://github.com/naren-m/get-started-openai.git
+cd get-started-openai
+
+python sample.py
+```
+
+
+## References
+
+- [Conda env creation for open ai](https://github.com/openai/universe-starter-agent)
+
+- [OpenAI Gym setup](https://gist.github.com/iambrian/2bcc8fc03eaecb2cbe53012d2f505465)
+
+- [Python programming tutorial on Open AI](https://www.youtube.com/watch?v=3zeg7H6cAJw)
+
---
+++ test.py
@@ -0,0 +1,97 @@
+# Source reference - https://pythonprogramming.net/openai-cartpole-neural-network-example-machine-learning-tutorial/
+
+import gym
+import random
+import numpy as np
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import median, mean
+from collections import Counter
+
+learning_rate = 1e-3
+
+env = gym.make('CartPole-v0')
+env.reset()
+
+goal_steps = 500
+score_requirement = 50
+initial_games = 1000
+
+def random_games():
+    for episode in range(5):
+        env.reset()
+        for t in range(goal_steps):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                break
+
+# random_games()
+
+def initial_population():
+    # [OBS, MOVES]
+    training_data = []
+    # all scores:
+    scores = []
+    # just the scores that met our threshold:
+    accepted_scores = []
+    # iterate through however many games we want:
+    for _ in range(initial_games):
+        score = 0
+        # moves specifically from this environment:
+        game_memory = []
+        # previous observation that we saw
+        prev_observation = []
+        # for each frame in 200
+        for _ in range(goal_steps):
+            # choose random action (0 or 1)
+            action = random.randrange(0,2)
+            # do it!
+            observation, reward, done, info = env.step(action)
+            
+            # notice that the observation is returned FROM the action
+            # so we'll store the previous observation here, pairing
+            # the prev observation to the action we'll take.
+            if len(prev_observation) > 0 :
+                game_memory.append([prev_observation, action])
+            prev_observation = observation
+            score+=reward
+            if done: break
+
+        # IF our score is higher than our threshold, we'd like to save
+        # every move we made
+        # NOTE the reinforcement methodology here. 
+        # all we're doing is reinforcing the score, we're not trying 
+        # to influence the machine in any way as to HOW that score is 
+        # reached.
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                # convert to one-hot (this is the output layer for our neural network)
+                if data[1] == 1:
+                    output = [0,1]
+                elif data[1] == 0:
+                    output = [1,0]
+                    
+                # saving our training data
+                training_data.append([data[0], output])
+
+        # reset env to play again
+        env.reset()
+        # save overall scores
+        scores.append(score)
+    
+    # just in case you wanted to reference later
+    training_data_save = np.array(training_data)
+    np.save('saved.npy',training_data_save)
+    
+    # some stats here, to further illustrate the neural network magic!
+    print('Average accepted score:',mean(accepted_scores))
+    print('Median score for accepted scores:',median(accepted_scores))
+    print(Counter(accepted_scores))
+    
+    return training_data
+
+initial_population()
